e43101394e9e22dfda367530b4217f78a374bb05
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-02-24 16:40:57 +0100

    printf exam v1.0

--- Exam03/ft_printf/ft_printf.c
+++ Exam03/ft_printf/ft_printf.c
@@ -1,26 +1,68 @@
 #include <unistd.h>
-#include <stdio.h>
+#include <stdarg.h>
 
-int	ft_putchar(char c, int *bytes)
+void	ft_putchar(char c, int *bytes)
 {
+	if (*bytes == -1)
+		return ;
 	if (write(1, &c, 1) == -1)
-		bytes = -1;
+		*bytes = -1;
 	else
-		*(bytes++);
+		(*bytes)++;
 }
 
-void	ft_fillformat(char *str, va_list lst, int *bytes)
+void	ft_putnbr(long n, char *str, int *bytes)
 {
-	//%s , %d , %x
-	if (str[*bytes + 1] == 's')
-		va_arg(lst, char);
-	else if (str[*bytes + 1] == 'd')
-		va_arg(lst, int);
-	else if (str[*bytes + 1] == 'x')
-		va_arg(lst, char *);
+	if (*bytes == -1)
+		return ;
+	if (n < 0)
+	{
+		ft_putchar('-', bytes);
+		n *= -1;
+	}
+	if (n < 10)
+		ft_putchar(str[n], bytes);
 	else
-		ft_putchar('%', bytes);
+	{
+		ft_putnbr(n / 10, str, bytes);
+		ft_putnbr(n % 10, str, bytes);
+	}
+}
 
+void	ft_puthex(unsigned int n, char *str, int *bytes)
+{
+	if (*bytes == -1)
+		return ;
+	if (n < 16)
+		ft_putchar(str[n], bytes);
+	else
+	{
+		ft_puthex(n / 16, str, bytes);
+		ft_puthex(n % 16, str, bytes);
+	}
+}
+
+void	ft_putstr(char *str, int *bytes)
+{
+	int	i;
+
+	i = 0;
+	if (*bytes == -1)
+		return ;
+	if (!str)
+		str = "(null)";
+	while (str[i])
+		ft_putchar(str[i++], bytes);
+}
+
+void	ft_fillformat(va_list lst, char c, int *bytes)
+{
+	if (c == 'd')
+		ft_putnbr(va_arg(lst, int), "0123456789", bytes);
+	else if (c == 's')
+		ft_putstr(va_arg(lst, char *), bytes);
+	else if (c == 'x')
+		ft_puthex(va_arg(lst, unsigned int), "0123456789abcdef", bytes);
 }
 
 int	ft_printf(char *str, ...)
@@ -35,25 +77,11 @@ int	ft_printf(char *str, ...)
 	while (str[i] && bytes != -1)
 	{
 		if (str[i] == '%')
-		{
-			ft_fillformat(str);
-		}
+			ft_fillformat(lst, str[++i], &bytes);
 		else
 			ft_putchar(str[i], &bytes);
+		i++;
 	}
-	i++;
 	va_end(lst);
 	return (bytes);
-}
-
-int	main(void)
-{
-	char	*str;
-	int		num;
-
-	str = "Hola";
-	num = 42;
-	ft_printf("%s from %d\n %d%d\n", str, num, num, num);
-	printf("%s from %d\n %d%d\n",str, num, num, num);
-	return (0);
 }
